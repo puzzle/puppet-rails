b64ab99b10b7ae37feb3a3359fd109458599f7f0
Marcel Haerry <haerry@puzzle.ch>
2008-05-19 15:00:28 +0200

    empty module $MODULENAME

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+#######################################
+# rails module
+# Puzzle ITC - haerry+puppet(at)puzzle.ch
+# GPLv3
+#######################################
+
+
+# modules_dir { "rails": }
+class rails {}
